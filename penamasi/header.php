d87f4e27659657784d69c0011caf671e8bb0ea66
bruno <Brunouasli@cupcode.com.br>
2019-06-25 01:23:06 -0300

    site penamasi

---
+++ penamasi/header.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<!-- For IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- For Resposive Device -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>PenaMasi</title>
+
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png">
+
+
+		<!-- Main style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- responsive style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+
+		<!-- Fix Internet Explorer ______________________________________-->
+
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="vendor/html5shiv.js"></script>
+			<script src="vendor/respond.js"></script>
+		<![endif]-->
+		<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "(+5519) 9 9178 0117  ", // WhatsApp number
+            call_to_action: "Gostaria de uma consultoria?", // Call to action
+            position: "left", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '337748510233001');
+	fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=337748510233001&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T346TRH');</script>
+<!-- End Google Tag Manager -->
+
+	</head>
+
+	<body>
+		<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T346TRH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+		<div class="main-page-wrapper">
+
+			<!-- ===================================================
+				Loading Transition
+			==================================================== -->
+			<div id="loader-wrapper">
+				<div id="loader"></div>
+			</div>
+
+
+
+			<!--
+			=============================================
+				Theme Header
+			==============================================
+			-->
+			<header class="theme-main-header">
+				<div class="top-header">
+					<div class="container">
+						<div class="clearfix">
+							<div class="float-left">
+								<ul class="left-widget">
+									<li>
+										<ul class="social-icon clearfix">
+											<li><a href="https://www.facebook.com/penamasiadvocaciaeconsultoria/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+											<li><a href="https://www.instagram.com/rogeriopenamasi/" target="_blank"><i class="fa fa-instagram" aria-hidden="true" ></i></a></li>
+											<li><a href="https://www.linkedin.com/in/rogerio-pena-masi-8a810629/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+							<div class="float-right">
+								<ul class="right-widget clearfix">
+									<li><a href="mailto:atendimento@penamasi.com.br"><i class="fa fa-envelope" aria-hidden="true"></i> atendimento@penamasi.com.br</a></li>
+									<li><a href="whatsapp://send?text=Olá!&phone=+5519991780117"><i class="fa fa-whatsapp" aria-hidden="true"></i> (19) 99178-0117</a></li>
+									<li class="quote"><a href="tel:+5519991780117">(19) 9 9178-0117</a></li>
+								</ul>
+							</div>
+						</div> <!-- /.clearfix -->
+					</div> <!-- /.container -->
+				</div> <!-- /.top-header -->
+
+
+
+				<div class="main-menu-wrapper clearfix">
+					<div class="container clearfix">
+						<!-- Logo -->
+						<div class="logo float-left"><a href="index.php"><img src="images/logo/logo2.png" alt="Logo"></a></div>
+
+
+
+						<!-- ============================ Theme Menu ========================= -->
+						<nav class="navbar-expand-lg float-right navbar-light" id="mega-menu-wrapper">
+					    	<button class="navbar-toggler float-right clearfix" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					    		<i class="flaticon-menu-options"></i>
+					    	</button>
+					    	<div class="collapse navbar-collapse clearfix" id="navbarNav">
+					    	  <ul class="navbar-nav nav">
+					    	    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+					    	    <li class="nav-item dropdown-holder">
+					    	    	<a class="nav-link" href="sobre.php">Sobre</a>
+					    	    </li>
+					    	    <li class="nav-item dropdown-holder">
+					    	    	<a class="nav-link" href="atuacao.php">Atuação</a>
+					    	    </li>
+					    	    <li class="nav-item dropdown-holder">
+					    	    	<a class="nav-link" href="/blog">Notícias</a>
+					    	    </li>
+					    	    <li class="nav-item dot-fix">
+					    	    	<a class="nav-link" href="contato.php">Contato</a>
+					    	    </li>
+					    	  </ul>
+					    	</div>
+						</nav>
+					</div> <!-- /.container -->
+				</div> <!-- /.main-menu-wrapper -->
+			</header> <!-- /.theme-main-header -->
---
+++ penamasi/noticias.php
@@ -0,0 +1,159 @@
+<?php $title = 'Sobre nós - penamasi'; include("header.php"); ?>
+			
+			<!-- 
+			=============================================
+				Theme Inner Banner
+			============================================== 
+			-->
+			<div class="inner-banner">
+				<div class="overlay">
+					<div class="container clearfix">
+						<h2>Notícias e Artigos</h2>
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li>/</li>
+							<li>Notícias</li>
+						</ul>
+					</div> <!-- /.container -->
+				</div> <!-- /.overlay -->
+			</div> <!-- /.inner-banner -->
+
+			
+			
+			<!-- 
+			=============================================
+				Our Blog
+			============================================== 
+			-->
+			<div class="our-blog">
+				<div class="container">
+					<div class="row">
+						<div class="col-xl-9 col-lg-8 col-12">
+							<div class="blog-post">
+								<div class="image"><img src="images/blog/10.jpg" alt=""></div>
+								<div class="date">September 13, 2017</div>
+								<h5 class="title"><a href="blog-details.html">The Advantages Minimal Repair Technique.</a></h5>
+								<p>Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex, quis venenatis orci tellus vel dolor. Donec gravida, dolor ut...</p>
+								<a href="#/" class="read-more">Read More</a>
+							</div> <!-- /.blog-post -->
+							<div class="blog-post">
+								<div class="image"><img src="images/blog/11.jpg" alt=""></div>
+								<div class="date">September 13, 2017</div>
+								<h5 class="title"><a href="#/">The Advantages Minimal Repair Technique.</a></h5>
+								<p>Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex, quis venenatis orci tellus vel dolor. Donec gravida, dolor ut...</p>
+								<a href="#/" class="read-more">Read More</a>
+							</div> <!-- /.blog-post -->
+							<div class="blog-post">
+								<div class="image"><img src="images/blog/12.jpg" alt=""></div>
+								<div class="date">September 13, 2017</div>
+								<h5 class="title"><a href="#/">The Advantages Minimal Repair Technique.</a></h5>
+								<p>Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex, quis venenatis orci tellus vel dolor. Donec gravida, dolor ut...</p>
+								<a href="#/" class="read-more">Read More</a>
+							</div> <!-- /.blog-post -->
+							<div class="blog-post">
+								<div class="image"><img src="images/blog/13.jpg" alt=""></div>
+								<div class="date">September 13, 2017</div>
+								<h5 class="title"><a href="#/">The Advantages Minimal Repair Technique.</a></h5>
+								<p>Sed rutrum, libero non pretium tristique, arcu mi sollicitudin ex, quis venenatis orci tellus vel dolor. Donec gravida, dolor ut...</p>
+								<a href="#/" class="read-more">Read More</a>
+							</div> <!-- /.blog-post -->
+
+
+							<div class="theme-pagination">
+								<ul>
+									<li class="active"><a href="#/">1</a></li>
+									<li><a href="#/">2</a></li>
+									<li><a href="#/">3</a></li>
+									<li><a href="#/">Next</a></li>
+								</ul>
+							</div> <!-- /.theme-pagination -->
+						</div> <!-- /.col- -->
+						<div class="col-xl-3 col-lg-4 col-md-6 col-sm-8 col-12 blog-sidebar">
+							<div class="sidebar-box sidebar-search">
+								<form action="#">
+									<input type="text" placeholder="Search..">
+									<button><i class="fa fa-search" aria-hidden="true"></i></button>
+								</form>
+							</div> <!-- /.sidebar-search -->
+							<div class="sidebar-box blog-categories">
+								<h3>Categoria</h3>
+								<ul>
+									<li><a href="#">Direito de Família </a></li>
+									<li><a href="#">Direito das Sucessões </a></li>
+									<li><a href="#">Direito Civil </a></li>
+									<li><a href="#">Direito Empresarial </a></li>
+									<li><a href="#">Direito Tributário </a></li>
+									<li><a href="#">Direito Médico </a></li>
+								</ul>
+							</div> <!-- /.blog-categories -->
+							<div class="sidebar-box sidebar-recent-post">
+								<h3>Recent News</h3>
+								<ul>
+									<li>
+										<a href="#/">Best website traffice Booster with  great tools.</a>
+										<span>12 May, 2016</span>
+									</li>
+									<li>
+										<a href="#/">Best website traffice Booster with  great tools.</a>
+										<span>12 May, 2016</span>
+									</li>
+									<li>
+										<a href="#/">Best website traffice Booster with  great tools.</a>
+										<span>12 May, 2016</span>
+									</li>
+								</ul>
+							</div> <!-- /.sidebar-recent-post -->
+							<div class="sidebar-keyword">
+								<h3>Keyword</h3>
+								<ul class="clearfix">
+									<li><a href="#/">Ideas</a></li>
+									<li><a href="#/">Education</a></li>
+									<li><a href="#/">design</a></li>
+									<li><a href="#/">animation</a></li>
+									<li><a href="#/">book</a></li>
+								</ul>
+							</div> <!-- /.sidebar-keyword -->
+						</div> <!-- /.blog-sidebar -->
+					</div> <!-- /.row -->
+				</div> <!-- /.container -->
+			</div> <!-- /.our-blog -->
+			
+
+
+
+			<!--
+			=====================================================
+				Footer
+			=====================================================
+			-->
+		<?php include 'footer.php'; ?>
+
+
+		<!-- Optional JavaScript _____________________________  -->
+
+    	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+		<script src="vendor/jquery.2.2.3.min.js"></script>
+		<!-- Popper js -->
+		<script src="vendor/popper.js/popper.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<!-- Language Stitcher -->
+		<script src="vendor/language-switcher/jquery.polyglot.language.switcher.js"></script>
+	    <!-- Mega menu  -->
+		<script src="vendor/bootstrap-mega-menu/js/menu.js"></script>
+		<!-- WOW js -->
+		<script src="vendor/WOW-master/dist/wow.min.js"></script>
+		<!-- owl.carousel -->
+		<script src="vendor/owl-carousel/owl.carousel.min.js"></script>
+		<!-- js count to -->
+		<script src="vendor/jquery.appear.js"></script>
+		<script src="vendor/jquery.countTo.js"></script>
+		<!-- Fancybox -->
+		<script src="vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+
+		<!-- Theme js -->
+		<script src="js/theme.js"></script>
+		</div> <!-- /.main-page-wrapper -->
+	</body>
+</html>
