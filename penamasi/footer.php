a9a7b969ed826d3698950441d2d62d0e6bc0a478
bruno <Brunouasli@cupcode.com.br>
2019-06-25 01:27:59 -0300

    penamasi

---
+++ penamasi/footer.php
@@ -0,0 +1,74 @@
+			<!--
+			=====================================================
+				Footer
+			=====================================================
+			-->
+			<footer class="theme-footer">
+				<div class="container">
+					<div class="content-wrapper">
+						<div class="footer-bottom-wrapper row">
+							<div class="col-lg-3 col-sm-6 col-12 footer-logo">
+								<div class="logo"><a href="index.php"><img src="images/logo/logo2.png" alt="Logo"></a></div>
+								<a "#" class="mail-address">Campinas - SP</a>
+							</div> <!-- /.footer-logo -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list">
+								<h4>MENU</h4>
+								<ul>
+									<li><a href="index.php">Home</a></li>
+									<li><a href="sobre.php">Sobre</a></li>
+									<li><a href="atuacao.php">Atuação</a></li>
+									<li><a href="noticias.php">Notícias</a></li>
+									<li><a href="contato.php">Contato</a></li>
+								</ul>
+							</div> <!-- /.footer-list -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list">
+								<h4>Horários</h4>
+								<p>Segunda à Sexta<br> das 8:30 às 18:30</p>
+							</div> <!-- /.footer-list -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list">
+								<h4>contato</h4>
+								<ul>
+									<li><a href="tel:+5519991780117">(19) 99178-0117</a></li>
+									<li><a href="mailto:atendimento@penamasi.com.br">atendimento@penamasi.com.br</a></li>
+								</ul>
+							</div> <!-- /.footer-list -->
+						</div> <!-- /.footer-bottom-wrapper -->
+
+						<div class="copyright-wrapper row">
+							<div class="col-md-8 col-sm-8 col-12">
+								<p>© 2019 <a href="#/">PenaMasi.</a> Todos os direitos reservados. - Desenvolvido por <a href="http://www.3mind.com.br" target="_blank">3mind</a></p>
+							</div>
+							<div class="col-md-4 col-sm-4 col-12">
+								<ul>
+									<li><a href="https://www.facebook.com/penamasiadvocaciaeconsultoria/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="https://www.instagram.com/rogeriopenamasi/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									<li><a href="https://www.linkedin.com/in/rogerio-pena-masi-8a810629/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div> <!-- /.copyright-wrapper -->
+					</div>
+				</div> <!-- /.container -->
+			</footer> <!-- /.theme-footer -->
+
+
+
+
+	        <!-- Scroll Top Button -->
+			<button class="scroll-top tran3s">
+				<i class="fa fa-angle-up" aria-hidden="true"></i>
+			</button>
+
+			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+		<script src="vendor/jquery.2.2.3.min.js"></script>
+			<script src="js/blog.js"></script>
+
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136106787-6"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-136106787-6');
+</script>
